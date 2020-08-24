0156bbf8b610d98cf00b9b69f9c4ba004f9f04f0
slawoe <info@slawomirernst.de>
2020-08-24 17:39:26 +0200

    Modulize password checker

--- cli.js
+++ cli.js
@@ -1,78 +1,38 @@
-const inquirer = require("inquirer");
-const fs = require("fs").promises;
+const {
+  askAccessQuestions,
+  askChoice,
+  askPasswordRequests,
+  askNewPassword,
+  CHOICE_GET,
+  CHOICE_SET,
+} = require("./lib/questions");
+const { readPassword } = require("./lib/passwords");
 
-const accessQuestions = [
-  {
-    type: "input",
-    name: "username",
-    message: "What's your username?",
-  },
-  {
-    type: "password",
-    name: "password",
-    message: "What's your masterpassword?",
-  },
-];
-
-const CHOICE_GET = "Get a password";
-const CHOICE_SET = "Set a password";
-
-const choice = [
-  {
-    type: "list",
-    name: "option",
-    message: "What do you want to do?",
-    choices: [CHOICE_GET, CHOICE_SET],
-  },
-];
-
-const passwordRequest = [
-  {
-    type: "input",
-    name: "key",
-    message: "Which password do you want to get?",
-  },
-];
-
-const newPassword = [
-  {
-    type: "input",
-    name: "topicPassword",
-    message: "What is the password for?",
-  },
-  {
-    type: "input",
-    name: "newPassword",
-    message: "What is the password?",
-  },
-];
-
-inquirer.prompt(accessQuestions).then((answers) => {
-  if (answers.password === "123" && answers.username === "Slawo") {
+async function main() {
+  const { masterPassword, username } = await askAccessQuestions();
+  if (masterPassword === "123" && username === "Slawo") {
     console.log("Welcome");
-    inquirer.prompt(choice).then(async (choice) => {
-      if (choice.option === CHOICE_GET) {
-        console.log("ok, buddy");
-        inquirer.prompt(passwordRequest).then(async (key) => {
-          try {
-            const passwordsJSON = await fs.readFile(
-              "./passwords.json",
-              "utf-8"
-            );
-            const passwords = JSON.parse(passwordsJSON);
-            console.log(
-              `Hi ${answers.username}, your needed password for ${key.key} is:
-                  ${passwords[key.key]}!`
-            );
-          } catch (error) {
-            console.error("Something went wrong 😑");
-          }
-        });
-      } else {
-        inquirer.prompt(newPassword).then((options) => {});
+    const { option } = await askChoice();
+    if (option === CHOICE_GET) {
+      console.log("Ok, buddy!");
+      const { key } = await askPasswordRequests();
+      try {
+        const password = await readPassword(key);
+        console.log(
+          `Hi ${username}, your needed password for ${key} is:
+                            ${password}!`
+        );
+      } catch (error) {
+        console.error("Something went wrong 😑");
       }
-    });
-  } else {
-    console.log(`Your password or username is wrong`);
-  }
-});
+    } else if (option === CHOICE_SET) {
+      console.log("Let's go my friend!");
+      const { title, password } = await askNewPassword();
+      console.log(
+        `You set up a password for ${title}. The new password is: ${password}`
+      );
+    }
+  } else console.log("Your password or unsername is wrong");
+}
+
+main();
