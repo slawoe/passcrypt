0b3ab4de316c730ef5baaa53b481cfc775f52272
slawoe <info@slawomirernst.de>
2020-08-26 12:24:29 +0200

    Add masterPassword-Question and data

--- cli.js
+++ cli.js
@@ -3,15 +3,29 @@ const {
   askChoice,
   askPasswordRequests,
   askNewPassword,
+  askForMasterPassword,
   CHOICE_GET,
   CHOICE_SET,
 } = require("./lib/questions");
-const { readPassword, writePassword } = require("./lib/passwords");
+const {
+  readPassword,
+  writePassword,
+  readMasterPassword,
+  writeMasterPassword,
+} = require("./lib/passwords");
 const { encrypt, decrypt, createHash, verifyHash } = require("./lib/crypto");
 
 async function main() {
+  const masterMasterPassword = await readMasterPassword();
+  if (!masterMasterPassword) {
+    const { newMasterPassword } = await askForMasterPassword();
+    await writeMasterPassword(newMasterPassword);
+    console.log("MP set");
+    return;
+  }
+
   const { masterPassword, username } = await askAccessQuestions();
-  if (masterPassword === "123" && username === "Slawo") {
+  if (masterPassword === masterMasterPassword && username === "Slawo") {
     console.log("Welcome");
     const { option } = await askChoice();
     if (option === CHOICE_GET) {
@@ -19,7 +33,7 @@ async function main() {
       const { key } = await askPasswordRequests();
       try {
         const password = await readPassword(key);
-        const decryptedPassword = decrypt(password, masterPassword);
+        const decryptedPassword = decrypt(password, masterMasterPassword);
         console.log(
           `Hi ${username}, your needed password for ${key} is: ${decryptedPassword}!`
         );
@@ -30,7 +44,7 @@ async function main() {
       console.log("Let's go my friend!");
       try {
         const { title, password } = await askNewPassword();
-        const encryptedPassword = encrypt(password, masterPassword);
+        const encryptedPassword = encrypt(password, masterMasterPassword);
         await writePassword(title, encryptedPassword);
         console.log(
           `You set up a password for ${title}. The new password is: ${password}`
