ff27599c33e39c90f271ad578ed0176091406dd6
slawoe <info@slawomirernst.de>
2020-08-27 10:56:40 +0200

    Add express

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.listen(3000, function () {
+  console.log("Listening on 3000");
+});
+
+app.get(`/`, function (req, res) {
+  res.sendFile("/Users/slawomirernst/dev/passcrypt" + "/index.html");
+});
