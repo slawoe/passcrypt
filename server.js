4f2351c21c2bac6ee223a0a1751e9d600c738997
slawoe <info@slawomirernst.de>
2020-08-27 20:03:08 +0200

    Add get and post

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
-const { writePassword, readPassword } = require("./lib/passwords");
+const {
+  writePassword,
+  readPassword,
+  deletePassword,
+} = require("./lib/passwords");
 const { encrypt, decrypt } = require("./lib/crypto");
 
 const client = new MongoClient(process.env.MONGO_URL, {
@@ -15,43 +19,58 @@ app.use(bodyParser.json());
 const port = 3000;
 
 async function main() {
-  try {
-    await client.connect();
-    const database = client.db(process.env.MONGO_DB);
-    const masterPassword = process.env.MASTER_PASSWORD;
+  await client.connect();
+  const database = client.db(process.env.MONGO_DB);
+  const masterPassword = process.env.MASTER_PASSWORD;
 
-    app.get("/api/passwords/:name", async (request, response) => {
-      try {
-        const { name } = request.params;
-        const password = await readPassword(name, database);
-        const decryptedPassword = decrypt(password, masterPassword);
-        response.status(200).send(decryptedPassword);
-      } catch (error) {
-        console.error("Something went wrong 😑", error);
-      }
-    });
+  app.get("/api/passwords/:name", async (request, response) => {
+    const { name } = request.params;
+    const password = await readPassword(name, database);
+    const decryptedPassword = decrypt(password, masterPassword);
+    response.status(200).send(decryptedPassword);
+  });
 
-    app.post("/api/passwords", async (request, response) => {
-      try {
-        console.log("POST in /api/passwords");
-        const { name, value } = request.body;
-        const encryptedPassword = encrypt(value, masterPassword);
-        await writePassword(name, encryptedPassword, database);
-        response.status(201).send("Password created");
-      } catch (error) {
-        console.error("Something went wrong 😑", error);
-      }
-    });
+  app.post("/api/passwords", async (request, response) => {
+    console.log("POST in /api/passwords");
+    const { name, value } = request.body;
+    const encryptedPassword = encrypt(value, masterPassword);
+    await writePassword(name, encryptedPassword, database);
+    response.status(201).send("Password created");
+  });
 
-    app.listen(port, function () {
-      console.log(`Listening on http://localhost:${port}`);
-    });
-  } finally {
-    await client.close();
-  }
+  app.listen(port, function () {
+    console.log(`Listening on http://localhost:${port}`);
+  });
 }
 
 main();
+
+// async function main() {
+//   await client.connect();
+//   const database = client.db(process.env.MONGO_DB);
+//   const masterPassword = process.env.MASTER_PASSWORD;
+//   app.get(`/api/passwords/amazon`, async (request, response) => {
+//     const key = "amazon";
+//     const encryptedPassword = await readPassword(key, database);
+//     const password = decrypt(encryptedPassword, masterPassword);
+//     response.send(password);
+//   });
+// }
+
+// app.post("api/passwords", (request, response) => {
+//   response.send("123");
+// });
+
+// app.get(`/`, function (request, response) {
+//   response.sendFile("/Users/slawomirernst/dev/passcrypt" + "/index.html");
+// });
+
+// app.listen(port, function () {
+//   console.log(`Listening on http://localhost:${port}`);
+// });
+// main();
+
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoClient.connect(
 //   process.env.MONGO_URL,
