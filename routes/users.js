9759762f86e1e96d55b16d2a06867ee3db0f7d23
slawoe <info@slawomirernst.de>
2020-08-31 15:16:25 +0200

    Find user by email and password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
@@ -7,30 +8,42 @@ const bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
 
+// function authenticateToken(resquest, response, next){
+//     const authHeader = request.headers[`authorization`]
+//     const token = authHeader && authHeader.split(``)[1]
+//     if(token == null)return res.sendStatus(401)
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: any, user: any) => {
+//         console.log(err)
+//         if (err) return res.sendStatus(403)
+//         request.user = user
+//     next()
+// }
+
 function createUsersRouter(database) {
   const collection = database.collection("users");
-  router.get("/", (request, response) => {
-    response.send("We`re on users");
+
+  router.post("/login", async (request, response) => {
+    try {
+      const { username, password } = request.body;
+      const user = await collection.findOne({
+        username,
+        password,
+      });
+      if (!user) {
+        response.status(401).send(`wrong username or password`);
+        return;
+      }
+      console.log(user);
+      response.send("Logged in");
+    } catch (error) {
+      console.error("Something went wrong 😑", error);
+      response.status(500).send(error.message);
+    }
   });
 
-  router.post("/:name", async (request, response) => {
-    const username = "Slawomir";
-    const password = "abcd";
-    if (username === "Slawomir" && password === "abc") {
-      try {
-        console.log(`Request /api/users/${request.params.name}`);
-        const user = await collection.findOne({
-          username: request.params.name,
-        });
-        response.json(user);
-      } catch (error) {
-        console.error("Something went wrong 😑", error);
-        response.status(500).send(error.message);
-      }
-    } else {
-      console.log("Password or username incorrect");
-      response.send("Password and username incorrect");
-    }
+  router.get("/", (request, response) => {
+    response.send("We`re on users");
   });
 
   return router;
